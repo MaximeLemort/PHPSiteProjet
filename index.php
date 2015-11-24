c10f63305c7a16c17dbb2b41ed611a5a4953886f
MaximeLemort <Mlemort@hotmail.fr>
2015-11-24 12:06:54 +0100

    Commit 2
    Petites modifs + test

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: maxime
- * Date: 11/19/2015
- * Time: 17:24
- */
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="index.css"/> <!--TODO-->
+        <title>Ceci est un blog</title>
+    </head>
+    <body>
+    <?php
+        <header>Blog</header>
+        $var='Pancakes';
+        echo $var.' gauffres<br>';
+    ?>
+    <footer>Ce blog est un projet PHP</footer>
+    </body>
+</html>
+
+
