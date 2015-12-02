a5e12ec1cbcbcf8dae2f495618837c3d8188205e
MaximeLemort <Mlemort@hotmail.fr>
2015-12-02 09:51:40 +0100

    Ajout page connection et header en table de liens

---
+++ vue/connection.php
@@ -0,0 +1,14 @@
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="stylesheet/bootstrap.css"/>
+        <title>Ceci est un début de blog</title>
+    </head>
+    <body>
+        <header class="page-header text-center">Test header</header>
+            <div> <!--Formulaire de connection-->
+
+            </div>
+        <footer class="modal-footer">Ce blog est un projet PHP</footer>
+    </body>
+</html>
